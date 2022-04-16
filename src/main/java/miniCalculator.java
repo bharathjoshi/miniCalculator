36ebb5a5cd7a27767cf1e14635801eb295f79c31
Bharath Joshi <bharathjoshi@Bharaths-MacBook-Air.local>
2022-04-16 17:06:06 +0530

    Updated source files

--- src/main/java/miniCalculator.java
+++ src/main/java/miniCalculator.java
@@ -1,23 +1,125 @@
-import java.lang.Math;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class miniCalculator {
-    public static double sqrt(int n){
+
+    private static final Logger logger = LogManager.getLogger(miniCalculator.class);
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public miniCalculator(){
+    }
+    public static double squareRootFunction(double n){
+        logger.info("[INFO]: Input for the Square Root function: " + n);
+        logger.info("[INFO]: Output of the Square Root function: " + Math.sqrt(n));
         return Math.sqrt(n);
     }
+
+    public static double LogFunction(double n)
+    {
+        logger.info("[INFO]: Input for the Natural Logarithm function: " + n);
+        logger.info("[INFO]: Output of the Natural Logarithm function: " + Math.log(n));
+        return Math.log(n);
+    }
+
+    public static double powerFunction(double n1, double n2)
+    {
+        logger.info("[INFO]: Input for the power function: " + n1 + " , "+ n2);
+        if(n1==0 && n2==0)
+        {
+            logger.info("[INFO]: Output of the power function: " + Double.NaN);
+            return Double.NaN;
+        }
+
+        logger.info("[INFO]: Output of the power function: " + Math.pow(n1, n2));
+        return Math.pow(n1, n2);
+    }
+
+    public static double factorialFunction(double n)
+    {
+
+        logger.info("[INFO]: Input for the Factorial function: " + n);
+        double res=1;
+
+        if(n < 0)
+            return Double.NaN;
+
+        for(int i=2;i<n+1;i++){
+            res*=i;
+        }
+        logger.info("[INFO]: Output of the Factorial function: "+ res);
+        return res;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Please select one of the below operations\nPress 1 for Square root function\nPress 2 for Factorial function\nPress 3 for Natural logarithm\nPress 4 for Power function\nPress 0 for exit\n");
-            int input = scanner.nextInt();
-            switch (input){
+
+        int option;
+        double num1, num2;
+        while(true){
+            System.out.println("-----------------------\n");
+            System.out.println("Please choose one of the below:\n1. For Square root\n2. For Factorial\n3. For Natural Logarithm\n4. For Power Function\n5. Exit");
+            try{
+                option = scanner.nextInt();
+            } catch (InputMismatchException error) {
+                logger.error("[ERROR]: Value type mismatch. Other type input instead of integer, Please try again.");
+                System.out.print("Only Decimal input is allowed");
+                return;
+            }
+            switch (option){
+
                 case 1:
-                    System.out.println("Enter a number for sqrt");
-                    int num = scanner.nextInt();
-                    System.out.println("\nThe sqrt of "+num+" is: "+sqrt(num));
+                    System.out.print("\nPlease enter a number for square root: ");
+                    num1 = scanner.nextDouble();
+                    if(num1 < 0)
+                    {
+                        System.out.println("please enter positive number\n");
+                        logger.error("[ERROR]: Square root is defined only for Positive numbers");
+                    }
+                    else
+                        System.out.println("The Square root of " + num1 + " is: " + squareRootFunction(num1));
+                    break;
+
+                case 2:
+                    System.out.print("\nPlease enter a number for factorial: ");
+                    num1 = scanner.nextInt();
+                    if (num1 < 0)
+                    {
+                        System.out.println("Please enter positive number\n");
+                        logger.error("[ERROR]: Factorial is defined only for Positive numbers");
+                        break;
+                    }
+                    else
+                        System.out.println("The Factorial of " + num1 + " is: " + factorialFunction(num1));
+                    break;
+
+                case 3:
+
+                    System.out.print("\nPlease enter number: ");
+                    num1 = scanner.nextDouble();
+                    if (num1 <= 0) {
+                        System.out.println("Please enter positive number\n");
+                        logger.error("[ERROR]: Logarithm is defined only for Positive numbers");
+                    }
+                    else {
+                        System.out.println("The Natural logarithm of " + num1 + " is: " + LogFunction(num1));
+                    }
+                    break;
+
+                case 4:
+                    System.out.print("\nPlease enter base: ");
+                    num1 = scanner.nextDouble();
+                    System.out.print("\nPlease enter exponent: ");
+                    num2 = scanner.nextDouble();
+                    System.out.println(num1 + " to the power " + num2 + " is: " + powerFunction(num1, num2));
+                    break;
                 case 5:
+                    System.out.println("Exited.");
                     return;
             }
         }
     }
+
+
+
 }
--- src/test/java/miniCalculatorTest.java
+++ src/test/java/miniCalculatorTest.java
@@ -1,18 +1,60 @@
+import static org.junit.Assert.*;
 import org.junit.Test;
-import org.junit.Assert;
 
 public class miniCalculatorTest {
     private miniCalculator testObj;
+    private static double DELTA = 1e-15;
 
     public miniCalculatorTest() {
         testObj = new miniCalculator();
     }
 
     @Test
-    public void sqrt(){
-        int num = 4;
-        double expResult = 2.0;
-        double result = testObj.sqrt(num);
-        Assert.assertEquals(expResult, result, 0);
+    public void squareRootTruePositive(){
+        assertEquals("SquareRoot test for True Positive", 4, testObj.squareRootFunction(16), DELTA);
+        assertEquals("SquareRoot test for True Positive", 16, testObj.squareRootFunction(256), DELTA);
     }
+
+    @Test
+    public void squareRootFalsePositive(){
+        assertNotEquals("SquareRoot test for False Positive", 4, testObj.squareRootFunction(4), DELTA);
+        assertNotEquals("SquareRoot test for False Positive", 16, testObj.squareRootFunction(16), DELTA);
+    }
+
+    @Test
+    public void naturalLogarithmTruePositive(){
+        assertEquals("Natural Logarithm test for True Positive", 2.302585092994046, testObj.LogFunction(10), DELTA);
+        assertEquals("Natural Logarithm test for True Positive", 4.605170185988091, testObj.LogFunction(100), DELTA);
+    }
+
+    @Test
+    public void naturalLogarithmFalsePositive(){
+        assertNotEquals("Natural Logarithm test for False Positive", 2.302585092994046, testObj.LogFunction(100), DELTA);
+        assertNotEquals("Natural Logarithm test for False Positive", -1, testObj.LogFunction(10), DELTA);
+    }
+
+    @Test
+    public void powerTruePositive(){
+        assertEquals("Power for True Positive", 16, testObj.powerFunction(4, 2), DELTA);
+        assertEquals("Power for True Positive",Double.NaN, testObj.powerFunction(0, 0), DELTA);
+    }
+
+    @Test
+    public void powerFalsePositive(){
+        assertNotEquals("Calculate Power for False Positive", 10, testObj.powerFunction(4, 2), DELTA);
+        assertNotEquals("Calculate Power for False Positive", 1000, testObj.powerFunction(0, 0), DELTA);
+    }
+
+    @Test
+    public void factorialTruePositive(){
+        assertEquals("Calculate Factorial for True Positive", 6, testObj.factorialFunction(3), DELTA);
+        assertEquals("Calculate Factorial for True Positive", 24, testObj.factorialFunction(4), DELTA);
+    }
+
+    @Test
+    public void factorialFalsePositive(){
+        assertNotEquals("Calculate Factorial for False Positive", 6, testObj.factorialFunction(2), DELTA);
+        assertNotEquals("Calculate Factorial for False Positive", 24, testObj.factorialFunction(0), DELTA);
+    }
+
 }
